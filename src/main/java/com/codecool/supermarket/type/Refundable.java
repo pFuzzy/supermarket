aa49b8f6a7e3389376a05640ee1896a0b37c4b6e
pFuzzy <p.fazii888@gmail.com>
2021-03-25 14:38:59 +0100

    fix refund day limit

--- src/main/java/com/codecool/supermarket/type/Refundable.java
+++ src/main/java/com/codecool/supermarket/type/Refundable.java
@@ -4,7 +4,7 @@ public interface Refundable {
     default int calculateRefundPrice(int originalPrice,int daysSincePurchase){
         if(daysSincePurchase == 0){
             return originalPrice;
-        }else if(daysSincePurchase < 50){
+        }else if(daysSincePurchase <= 50){
             return originalPrice/ 2;
         }
         return 0;
