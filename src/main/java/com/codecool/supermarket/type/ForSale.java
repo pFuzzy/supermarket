7828bdc178bec0fa43b253071f1c5f7eec574c9c
pFuzzy <p.fazii888@gmail.com>
2021-03-25 12:55:08 +0100

    add product types as interfaces

---
+++ src/main/java/com/codecool/supermarket/type/ForSale.java
@@ -0,0 +1,5 @@
+package com.codecool.supermarket.type;
+
+public interface ForSale {
+    int calculatePrice();
+}
---
+++ src/main/java/com/codecool/supermarket/type/Refundable.java
@@ -0,0 +1,5 @@
+package com.codecool.supermarket.type;
+
+public interface Refundable {
+    int calculateRefundPrice();
+}
